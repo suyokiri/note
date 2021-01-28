446bec7a353373edf03c40f5c59e7fdbc64854d4
roixxv <62254177+suyokiri@users.noreply.github.com>
2021-01-28 03:24:55 +0100

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-
-  const buttons = document.querySelectorAll('.faq-toggle');
-
-buttons.forEach(button => { button.parentNode.addEventListener('click', () => {
-    button.parentNode.classList.toggle('active');
-    console.log(button.classList);
-  })
-})
